af8075e1bd1740f9b03797100c7de500481238f2
chaoukis <sammy.chaouki@dawsoncollege.qc.ca>
2017-08-28 09:17:22 -0400

    Create and Test Address

---
+++ Common/test/lib/AddressTest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package lib;
+
+/**
+ * @author 1532038
+ *
+ */
+public class AddressTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Address myAddress = new Address("3040", "Sherbrooke");
+
+		System.out.println(myAddress.getAddress());
+
+	}
+
+}
